5ad7305e94198d586ad05de115abe1e875159ef3
GGroup <nikitkasarkisov@yandex.ru>
2025-06-12 18:10:33 +0700

    Update saveCookies.js

--- saveCookies.js
+++ saveCookies.js
@@ -1,30 +1,21 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true }); // можно поставить false для визуализации
+  const username = 'nicksmartposter';
+  const cookiesPath = `cookies/${username}.json`;
+
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
   const page = await browser.newPage();
 
   await page.goto('https://www.instagram.com/accounts/login/', { waitUntil: 'networkidle2' });
+  console.log('⏳ Войдите вручную. У тебя 60 сек...');
+  await page.waitForTimeout(60000);
 
-  // Вводим логин и пароль
-  await page.type('input[name="username"]', 'nicksmartposter', { delay: 100 });
-  await page.type('input[name="password"]', '0RrjZIx1q4', { delay: 100 });
-
-  // Нажимаем кнопку логина и ждем загрузки
-  await Promise.all([
-    page.click('button[type="submit"]'),
-    page.waitForNavigation({ waitUntil: 'networkidle2' }),
-  ]);
-
-  // Опционально: можно добавить ожидание, чтобы убедиться, что загрузилась главная страница
-  // await page.waitForSelector('nav'); 
-
-  // Сохраняем куки в файл
   const cookies = await page.cookies();
-  await fs.writeFile('cookies.json', JSON.stringify(cookies, null, 2));
+  if (!fs.existsSync('cookies')) fs.mkdirSync('cookies');
+  fs.writeFileSync(cookiesPath, JSON.stringify(cookies, null, 2));
 
-  console.log('Куки сохранены в cookies.json');
-
+  console.log(`✅ Куки сохранены в ${cookiesPath}`);
   await browser.close();
 })();
