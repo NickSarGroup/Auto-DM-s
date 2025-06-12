bb0d9423d7f1f2617ae654b7b1a2207ccc1be36b
GGroup <nikitkasarkisov@yandex.ru>
2025-06-13 01:08:16 +0700

    Update saveCookies.js

--- saveCookies.js
+++ saveCookies.js
@@ -1,21 +1,18 @@
-const puppeteer = require('puppeteer');
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const username = 'nicksmartposter';
-  const cookiesPath = `cookies/${username}.json`;
-
   const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
   const page = await browser.newPage();
 
   await page.goto('https://www.instagram.com/accounts/login/', { waitUntil: 'networkidle2' });
-  console.log('⏳ Войдите вручную. У тебя 60 сек...');
-  await page.waitForTimeout(60000);
+
+  console.log('⏳ Войди в аккаунт. У тебя 90 сек...');
+  await page.waitForTimeout(90000); // больше времени на вход
 
   const cookies = await page.cookies();
-  if (!fs.existsSync('cookies')) fs.mkdirSync('cookies');
-  fs.writeFileSync(cookiesPath, JSON.stringify(cookies, null, 2));
+  fs.writeFileSync('cookies.json', JSON.stringify(cookies, null, 2));
 
-  console.log(`✅ Куки сохранены в ${cookiesPath}`);
+  console.log('✅ Куки сохранены в cookies.json');
   await browser.close();
 })();
